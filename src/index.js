9acc3108ea69a6f16b3cf51a49152d42f4b4e792
Ivan Knyazev <knyazev13ivan@gmail.com>
2020-05-10 14:37:04 +0300

    -no-edit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const sum = (a, b) => a + b;
-const num = 3;
-console.log(sum(2, num));
+/* eslint-disable */
+"use strict"
+// eslint-disable-next-line no-unused-vars
+const bench = (func, arg, iterations = 1e6) => {
+  let t = performance.now();
+  for (let q = 0; q < iterations; q += 1) {
+    func(arg);
+  }
+  t = performance.now() - t;
+  console.log(`// ${t.toFixed(3)} ms`);
+};
 
-export default sum;
+const make = () => {
+  let word = 'wow!';
+  
+  
+  console.log(
+
+  );
+};
+// eslint-disable-next-line eol-last
+make();
